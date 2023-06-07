a0ac0fb49bf7f1c729f34b5b8e53a69a06b326ab
luigisaetta <luigi.saetta@gmail.com>
2023-06-07 14:08:27 +0000

    added sh files

---
+++ create_tar.sh
@@ -0,0 +1,2 @@
+# tar -czvf test1.tar.gz test1.sh test1.py
+tar -czvf test1.tar.gz train.sh lightning_mnist_cnn.py
---
+++ train.sh
@@ -0,0 +1,4 @@
+# Modern version, based on Torchrun
+# max_steps="800"
+# warmup_steps="200"
+python decompressed_artifact/lightning_mnist_cnn.py
